4f4356ff096a8ed5efaf010d4c0b9a1501908ee6
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 16:21:12 -0600

    name for each practice

--- iteration.js
+++ iteration.js
@@ -1,15 +1,53 @@
-const fruits = ['apple', "pear", "cherry"];
+const fruits = ["apple", "pear", "cherry"];
 
 
 //for loop iteration
-for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
-}
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
 
-//forEach iteration
-fruits.forEach(function(fruit) {
-  console.log(fruit);
-})
+// //forEach iteration
+// fruits.forEach(function(fruit) {
+//   console.log(fruit);
+// })
 
-//forEach one line
-fruits.forEach(fruit => console.log(fruit));
+// //forEach one line
+// fruits.forEach(fruit => console.log(fruit));
+
+
+
+
+//capitalize with forEach
+// let capitalizedFruits = [];
+// fruits.forEach(fruit => {
+//   let capitalizedFruit = fruit.toUpperCase();
+//   capitalizedFruits.push(capitalizedFruit);
+// });
+
+// console.log(capitalizedFruits);
+
+
+
+
+// const prices = [6.75, 3.10, 4.00, 8.12]; //total 21.97
+
+// let total = 0;
+// prices.forEach(price => {
+//   total += price;
+// })
+
+// console.log(total);
+
+
+
+
+
+// const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+// let sNames = [];
+// names.forEach(name => {
+//   if (name[0] === 'S') {
+//     sNames.push(name)
+//   }
+// });
+
+// console.log(sNames);
