62481282ee417b525678f3da3b7380b3db41ea69
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 16:06:33 -0600

    forEach method example

--- iteration.js
+++ iteration.js
@@ -1,5 +1,15 @@
 const fruits = ['apple', "pear", "cherry"];
 
+
+//for loop iteration
 for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
+
+//forEach iteration
+fruits.forEach(function(fruit) {
+  console.log(fruit);
+})
+
+//forEach one line
+fruits.forEach(fruit => console.log(fruit));
