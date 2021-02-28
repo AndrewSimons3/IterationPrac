b3a843fd1a496e1734a46a9ae2cef3f4fe8a9f70
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 17:55:40 -0600

    over 50 for each

--- forEach.js
+++ forEach.js
@@ -117,3 +117,21 @@
 // });
 
 // console.log(noel);
+
+
+
+
+//PROBLEM 5 - iterate over percentages and store all percentages over 50% in the array
+const percentages = [34, 67, 12, 39, 90, 82, 22, 24, 99];
+let upperRange = [];
+
+// upperRange should be: [67, 90, 82, 99]
+// Write your code below
+
+percentages.forEach(percentage => {
+  if (percentage > 50) {
+    upperRange.push(percentage)
+  }
+});
+
+console.log(upperRange);
