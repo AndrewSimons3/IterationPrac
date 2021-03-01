e0ad07aac9f753149edaf21992804d2469c7ff5a
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 21:19:50 -0600

    map daysofweek abbreviation

--- filter.js
+++ filter.js
@@ -16,12 +16,35 @@
 
 
 
-const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+// const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+// // century20 should be: [1989, 2000, 1999, 1973]
+// // Write your code below
+// century20 = years.filter(year => year <= 2000);
+// console.log(century20)
 
 
-// century20 should be: [1989, 2000, 1999, 1973]
-// Write your code below
 
-century20 = years.filter(year => year <= 2000);
+// const strings = ['1','2','3','4','5'];
+// let numbers = strings.map(string => parseInt(string, 10));
+// console.log(numbers);
 
-console.log(century20)
+
+
+// const fruits = ['apple', 'pear', 'cherry'];
+// const capitalizedFruits = fruits.map(fruit => fruit.toUpperCase());
+// console.log(capitalizedFruits);
+
+
+
+// const prices = [5, 4.23, 6.4, 8.09, 3.20];
+// const priceToDollars = price => `$${price.toFixed(2)}`
+// const stringPrice = prices.map(priceToDollars);
+// console.log(stringPrice)
+
+
+// const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let abbreviatedDays;
+// // abbreviatedDays should be: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// // Write your code below
+// abbreviatedDays = daysOfWeek.map(days => days.slice(0, 3));
+// console.log(abbreviatedDays);
