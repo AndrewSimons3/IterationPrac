3021f31533d98dd248e37f0294dae98833fd05c6
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 20:52:03 -0600

    even number filter

--- filter.js
+++ filter.js
@@ -1,9 +1,18 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10];
+// //EVEN NUMBER FILTER
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
 
-const evenNumbers = numbers.filter(number => {
-  if (number % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-});
+// const evenNumbers = numbers.filter(number => {
+//   if (number % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// //ONE LINE SOLUTION
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+// console.log(evenNumbers)
+
+
+
