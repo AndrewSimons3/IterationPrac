89fe9f562d0071f8755ab2fc1a6e7673d57977e6
AndrewSimons3 <simdrew07@gmail.com>
2021-02-28 21:00:14 -0600

    filter 20th century years

--- filter.js
+++ filter.js
@@ -16,3 +16,12 @@
 
 
 
+const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+
+
+// century20 should be: [1989, 2000, 1999, 1973]
+// Write your code below
+
+century20 = years.filter(year => year <= 2000);
+
+console.log(century20)
