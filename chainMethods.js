46bd9d4d7447931b1feaf49d425bf09bdbad6c15
AndrewSimons3 <simdrew07@gmail.com>
2021-03-02 07:18:26 -0600

    filter and reduce example

--- chainMethods.js
+++ chainMethods.js
@@ -129,7 +129,27 @@
 
 
 
+const products = [
+  { name: 'hard drive', price: 59.99 },
+  { name: 'lighbulbs', price: 2.59 },
+  { name: 'paper towels', price: 6.99 },
+  { name: 'flatscreen monitor', price: 159.99 },
+  { name: 'cable ties', price: 19.99 },
+  { name: 'ballpoint pens', price: 4.49 }
+];
 
+    // Result: { name: 'paper towels', price: 6.99 }
+
+    const newProduct = products
+    .filter(product => product.price < 10)
+    .reduce((highest, product) => {
+      if (highest.price > product.price) {
+        return highest;
+      }
+      return product;
+    });
+
+    console.log(newProduct)
 
 
 
