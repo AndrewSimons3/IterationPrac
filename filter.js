405c97ae0efe4b7bd32e46e9955e7f7146a2b56c
AndrewSimons3 <simdrew07@gmail.com>
2021-03-01 20:45:54 -0600

    using filter and map

--- filter.js
+++ filter.js
@@ -66,16 +66,16 @@
 
 
 
-const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
-let numberOf503;
+// const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
+// let numberOf503;
 
-// numberOf503 should be: 3
-// Write your code below
+// // numberOf503 should be: 3
+// // Write your code below
 
-numberOf503 = phoneNumbers.reduce((count, num) => {
-  if (num.startsWith('(503)')) {
-    return count + 1
-  } return count
-}, 0);
+// numberOf503 = phoneNumbers.reduce((count, num) => {
+//   if (num.startsWith('(503)')) {
+//     return count + 1
+//   } return count
+// }, 0);
 
-console.log(numberOf503)
+// console.log(numberOf503)
