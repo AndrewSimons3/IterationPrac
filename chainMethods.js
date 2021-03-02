32038774a27daa88eca5b4db312e70352c527b1a
AndrewSimons3 <simdrew07@gmail.com>
2021-03-01 21:43:37 -0600

    todo problem

--- chainMethods.js
+++ chainMethods.js
@@ -88,3 +88,36 @@
 
 
 
+
+
+
+const todos = [
+  {
+      todo: 'Buy apples',
+      done: false
+  },
+  {
+      todo: 'Wash car',
+      done: true
+  },
+  {
+      todo: 'Write web app',
+      done: false
+  },
+  {
+      todo: 'Read MDN page on JavaScript arrays',
+      done: true
+  },
+  {
+      todo: 'Call mom',
+      done: false
+  }
+];
+let unfinishedTasks;
+
+// unfinishedTasks should be: ["Buy apples", "Write web app", "Call mom"]
+// Write your code below
+
+
+
+
