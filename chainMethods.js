f4ecc484b22cef7b6ba547f6fed1390b00dde7f2
AndrewSimons3 <simdrew07@gmail.com>
2021-03-01 21:48:03 -0600

    completed todo challenge

--- chainMethods.js
+++ chainMethods.js
@@ -91,32 +91,45 @@
 
 
 
-const todos = [
-  {
-      todo: 'Buy apples',
-      done: false
-  },
-  {
-      todo: 'Wash car',
-      done: true
-  },
-  {
-      todo: 'Write web app',
-      done: false
-  },
-  {
-      todo: 'Read MDN page on JavaScript arrays',
-      done: true
-  },
-  {
-      todo: 'Call mom',
-      done: false
-  }
-];
-let unfinishedTasks;
+// const todos = [
+//   {
+//       todo: 'Buy apples',
+//       done: false
+//   },
+//   {
+//       todo: 'Wash car',
+//       done: true
+//   },
+//   {
+//       todo: 'Write web app',
+//       done: false
+//   },
+//   {
+//       todo: 'Read MDN page on JavaScript arrays',
+//       done: true
+//   },
+//   {
+//       todo: 'Call mom',
+//       done: false
+//   }
+// ];
+// let unfinishedTasks;
 
-// unfinishedTasks should be: ["Buy apples", "Write web app", "Call mom"]
-// Write your code below
+// // unfinishedTasks should be: ["Buy apples", "Write web app", "Call mom"]
+// // Write your code below
+
+// unfinishedTasks = todos
+// .filter(todo => todo.done === false)
+// .map(todo => todo.todo)
+
+// console.log(unfinishedTasks);
+
+
+
+
+
+
+
 
 
 
