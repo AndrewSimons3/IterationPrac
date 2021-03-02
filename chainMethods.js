7f8a433b57fb25a11ec82904c2f1a989390a52fc
AndrewSimons3 <simdrew07@gmail.com>
2021-03-01 21:33:02 -0600

    filter and map chain

--- chainMethods.js
+++ chainMethods.js
@@ -26,3 +26,58 @@
 // }, {});
 
 // console.log(usersObject)
+
+
+
+
+
+// const authors = [
+//   { firstName: "Beatrix", lastName: "Potter" },
+//   { firstName: "Ann", lastName: "Martin" },
+//   { firstName: "Beverly", lastName: "Cleary" },
+//   { firstName: "Roald", lastName: "Dahl" },
+//   { firstName: "Lewis", lastName: "Carroll" }
+// ];
+// let fullAuthorNames;
+
+// // fullAuthorNames should be: ["Beatrix Potter", "Ann Martin", "Beverly Cleary", "Roald Dahl", "Lewis Carroll"]
+// // Write your code below
+
+// fullAuthorNames = authors
+// .map(authors => `${authors.firstName} ${authors.lastName}`);
+// console.log(fullAuthorNames);
+
+
+
+
+
+
+
+
+
+// const userNames = ['Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean'];
+
+// const users = userNames
+// .filter(userName => userName[0] === "S")
+// .map(userName => ({ userName }));
+
+// console.log(users);
+
+
+
+
+
+
+
+const users = [
+  {name: 'Samir', age: 27},
+  {name: 'Angela', age: 33},
+  {name: 'Beatrice', age: 42},
+  {name: 'Shaniqua', age: 30},
+  {name: 'Marvin', age: 23},
+  {name: 'Sean', age: 47}
+];
+
+
+
+
